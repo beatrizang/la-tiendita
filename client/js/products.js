719d5707788d9a52b3853eabd82a1e36f6bc4c2b
BeatrizG <beatrizgarciaang0@gmail.com>
2023-10-03 18:29:08 -0300

    Se agrega parte de la estructura html con los estilos correspondientes

---
+++ client/js/products.js
@@ -0,0 +1,79 @@
+const products = [
+    {
+        id: 1,
+        name: "bermuda cargo",
+        price: 15500.30,
+        quantity: 1,
+        img: "./images/bermuda-cargo.webp",
+    },
+    {
+        id:2,
+        name:"campera jean",
+        price: 35687.50,
+        quantity:1,
+        img:"./images/campera-jean.webp",
+    },
+    {
+        id:3,
+        name:"gorra",
+        price:5682.10,
+        quantity:1,
+        img:"./iamges/gorra-verde.webp",
+    },
+
+    {
+        id:4,
+        name:"musculosa",
+        price: 8750.30,
+        quantity:1,
+        img:"./images/musculosa-blanca.webp",
+    },
+
+    {
+        id:5,
+        name:"pantalón cargo",
+        price:32658.00,
+        quantity:1,
+        img:"./images/pantalon-cargo.webp",
+    },
+
+    {
+        id:6,
+        name:"piluso",
+        price: 7856.25,
+        quantity:1,
+        img:"./images/piluso-crema.webp",
+    },
+
+    {
+        id:7,
+        name:"remera north face",
+        price:12547.10,
+        quantity:1,
+        img:"./iamges/remera-negra-nf",
+    },
+
+    {
+        id:8,
+        name:"remera vans",
+        price:15000.00,
+        quantity:1,
+        img:"./images/remera-negra-vans.webp",
+    },
+
+    {
+        id:9,
+        name:"zapatilla nike air force",
+        price:87999.56,
+        quantity:1,
+        img:"./images/zapatilla-nike-air-force.jpg",
+    },
+
+    {
+        id:10,
+        name:"zapatilla vans old skull",
+        price:65324.56,
+        quantity:1,
+        img:"./images/zapatilla-vans-old-skull.webp",
+    },
+    ];
