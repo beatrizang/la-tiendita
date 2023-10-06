6677ff2910cb9fc297bc7eeb57d6148895682c41
BeatrizG <beatrizgarciaang0@gmail.com>
2023-10-06 17:52:29 -0300

    Se termina la funcionalidad del carrito

--- client/js/index.js
+++ client/js/index.js
@@ -15,4 +15,26 @@ products.forEach((product) => {
     buyButton.className = 'product-btn';
     buyButton.innerText = 'comprar';
     content.append(buyButton);
+
+    buyButton.addEventListener('click', () => {
+        const repeat = cart.some((repeatProduct) => repeatProduct.id === product.id);
+        if(repeat){
+            cart.map((prod) => {
+                if(prod.id === product.id){
+                    prod.quantity++;
+                    displayCartCounter();
+                }
+            })
+        }
+        else{
+            cart.push({
+                id:product.id,
+                name:product.name,
+                price:product.price,
+                quantity:product.quantity,
+                img:product.img,
+            });
+            displayCartCounter();
+        }
+    })
 });
